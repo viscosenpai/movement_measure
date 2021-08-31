1429ac81fff495dcc56412a7ebc219a9240d3785
viscosenpai <kama.hiroki@gmail.com>
2021-09-01 00:37:39 +0900

    [add] Historyの見栄えを整える

--- lib/widgets/record_card.dart
+++ lib/widgets/record_card.dart
@@ -18,48 +18,45 @@ class RecordCard extends StatelessWidget {
       color: Color(0x99000000),
       elevation: 0.8,
       margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
-      child: ListTile(
-        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-        title: Container(
-          padding: EdgeInsets.only(right: 10.0),
-          decoration: BoxDecoration(
-            border: Border(
-              right: BorderSide(
-                width: 1.0,
-                color: Colors.white,
-              ),
+      child: InkWell(
+        splashColor: Colors.orangeAccent,
+        onTap: () {},
+        child: ListTile(
+          contentPadding:
+              EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          title: Container(
+            padding: EdgeInsets.only(right: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  '$dateTime',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '$time',
+                      style: TextStyle(color: Colors.white, fontSize: 24.0),
+                    ),
+                    Text(
+                      '$distance m',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                '$dateTime',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '$time',
-                    style: TextStyle(color: Colors.white, fontSize: 24.0),
-                  ),
-                  Text(
-                    '$distance m',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          trailing:
+              Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
         ),
-        trailing:
-            Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
       ),
     );
   }
