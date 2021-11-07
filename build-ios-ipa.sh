77dc69e738675e6a94980323a120f8b451f62506
viscosenpai <kama.hiroki@gmail.com>
2021-11-07 12:42:31 +0900

    [add] iosビルドスクリプトを修正

--- build-ios-ipa.sh
+++ build-ios-ipa.sh
@@ -17,24 +17,30 @@ flutter clean
 # 依存関係を解決
 flutter pub get
 
-# flutter buildを実行
-flutter build ios --release \
-  --build-name=${TARGET_APP_VERSION} --build-number=${TARGET_BUILD_NO}
-
-cd ios
-
 # archiveを作成
-EXPORT_OPTION_PLIST="ExportOptions.plist"
+# EXPORT_OPTION_PLIST="ExportOptions.plist"
+# EXPORT_OPTION_PLIST="ExportOptionsProd.plist"
 
-xcodebuild -workspace Runner.xcworkspace -scheme Runner -sdk iphoneos \
-  -configuration Release archive -archivePath ../build/Runner.xcarchive
-
+# flutter buildを実行
 # ipa ファイルを作成
-xcodebuild -allowProvisioningUpdates -exportArchive \
-  -archivePath ../build/Runner.xcarchive \
-  -exportOptionsPlist ${EXPORT_OPTION_PLIST} -exportPath ../build/ios-release
+# flutter build ios --release \
+#   --build-name=${TARGET_APP_VERSION} --build-number=${TARGET_BUILD_NO}
 
-cd ..
+flutter build ipa --release \
+  --build-name=${TARGET_APP_VERSION} --build-number=${TARGET_BUILD_NO}
+  --export-options-plist="ios/ExportOptionsProd.plist"
+
+# cd ios
+
+# xcodebuild -workspace Runner.xcworkspace -scheme Runner -sdk iphoneos \
+#   -configuration Release archive -archivePath ../build/Runner.xcarchive
+
+# # ipa ファイルを作成
+# xcodebuild -allowProvisioningUpdates -exportArchive \
+#   -archivePath ../build/Runner.xcarchive \
+#   -exportOptionsPlist ${EXPORT_OPTION_PLIST} -exportPath ../build/ios-release
+
+# cd ..
 
 # 成果物をコピー
-cp ./build/ios-release/movement_measure.ipa ${OUTPUT_APK_PATH}/app-release-${TARGET_APP_VERSION}.${TARGET_BUILD_NO}.ipa
+cp ./build/ios/ipa/movement_measure.ipa ${OUTPUT_APK_PATH}/app-release-${TARGET_APP_VERSION}.${TARGET_BUILD_NO}.ipa
